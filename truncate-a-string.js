194a63fc005f9f3fb60f08f0ce4a795981a12dc4
S M Abdullah Al Sabir <sabir.smaa2@gmail.com>
2021-05-15 15:14:03 +0200

    truncate-a-string

---
+++ truncate-a-string.js
@@ -0,0 +1,12 @@
+function truncateString(str, num) {
+  let newStr ="";
+  if(str.length > num){
+    newStr = str.slice(0, num) + "...";
+  }
+  else{
+    newStr = str;
+  }
+  return newStr;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
